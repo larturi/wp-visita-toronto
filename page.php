08b2f8180efed15ec04a1e8bd4e57a493a463776
larturi <lea.arturi@gmail.com>
2022-01-26 21:28:04 -0300

    Sidebar

--- page.php
+++ page.php
@@ -1,11 +1,14 @@
 <?php get_header(); ?>
 
-<?php while ( have_posts() ) : the_post(); ?>
+<div id="primary" class="primary">
+    <?php 
+        while ( have_posts() ) : the_post();
+            the_title();
+            the_content(); 
+        endwhile; 
+    ?>
+</div>
 
-    <?php the_title(); ?>
-
-    <?php the_content(); ?>
-
-<?php endwhile; ?>
+<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
---
+++ sidebar.php
@@ -0,0 +1,5 @@
+<div id="secondary" class="secondary">
+    Sidebar aqui
+</div>
+
+<div class="clear"></div>
