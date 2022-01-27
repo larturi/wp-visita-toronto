de39a5e0c4204233c0db40f3880a9e08cfc91a4e
larturi <lea.arturi@gmail.com>
2022-01-26 21:21:12 -0300

    Footer

--- footer.php
+++ footer.php
@@ -1,9 +1,24 @@
 
         </div>
         <!-- End Content -->
-
             <footer class="site-footer" role="contentinfo">
-                Desde el footer
+                <div class="container">
+                    <nav id="footer-navigation" class="footer-navigation">
+                        <?php 
+                            wp_nav_menu(array(
+                                'theme_location' => 'menu_principal',
+                                'menu_id' => 'menu-principal',
+                                'menu_class' => 'menu-principal'
+                            )); 
+                        ?>
+                    </nav>
+                </div>
+
+                <div class="clear"></div>
+
+                <div class="copyright">
+                    <p>Visita Toronto <?php echo date('Y') ?></p>
+                </div>
             </footer>
         
         </div>
