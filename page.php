42a8f367e40434a769a5df64cc40a6f1b8dd4b36
larturi <lea.arturi@gmail.com>
2022-01-27 22:03:42 -0300

    Fix para pages sin imagen destacada

--- page.php
+++ page.php
@@ -2,10 +2,14 @@
 
     <?php while ( have_posts() ) : the_post(); ?>
 
-        <div class="destacada">
-            <?php the_post_thumbnail('destacada'); ?>
-            <h2><?php the_title(); ?></h2>
-        </div>
+        <?php if (has_post_thumbnail()) { ?>
+            <div class="destacada">
+                <?php the_post_thumbnail('destacada'); ?>
+                <h2><?php the_title(); ?></h2>
+            </div>
+        <?php } else { ?>
+            <h2 class="noimagen"><?php the_title(); ?></h2>
+        <?php } ?>
          
         <div id="primary" class="primary">
             <?php the_content(); ?>
